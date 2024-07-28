6dbace475ccd5016bb2323c10dc5260161d9ccb5
Max Martinevsky <max@martinevsky.ru>
2024-07-28 17:31:55 +0300

    snap tap

--- keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
@@ -22,6 +22,14 @@ enum layers{
     WIN_FN,
 };
 
+// (C) https://www.reddit.com/r/MechanicalKeyboards/comments/1ec88th/comment/lfabt95/
+
+enum user_keycodes 
+{
+  SOCD_A = SAFE_RANGE,
+  SOCD_D
+};
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
@@ -30,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
         _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        _______,  KC_CAPS,  SOCD_A,   KC_S,     SOCD_D,   KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -67,3 +75,49 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
 };
 #endif // ENCODER_MAP_ENABLE
+
+bool a_down = false;
+bool d_down = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) 
+{
+    switch (keycode) 
+    {
+        case SOCD_A:
+            a_down = record->event.pressed;
+            if (record->event.pressed) 
+            {
+                if (d_down) 
+                    unregister_code(KC_D);
+                
+                register_code(KC_A);
+            } 
+            else 
+            {
+                unregister_code(KC_A);
+
+                if (d_down) 
+                    register_code(KC_D);
+            }
+        return false;
+
+        case SOCD_D:
+            d_down = record->event.pressed;
+            if (record->event.pressed) 
+            {
+                if (a_down) 
+                    unregister_code(KC_A);
+                
+                register_code(KC_D);
+            } 
+            else 
+            {
+                unregister_code(KC_D);
+
+                if (a_down) 
+                    register_code(KC_A);
+            }
+            return false;
+    }
+    return true;
+}
